478aeabc14a8b706a39d775384b80f88e258630f
TuomasHirv <hirvonentume@gmail.com>
2026-05-20 14:18:28 +0300

    whoah

---
+++ Osa 4/teht4.3-4.5/utils/list_helper.js
@@ -0,0 +1,50 @@
+const dummy = (blogs) => {
+    return 1
+  }
+  
+const totalLikes = (blogs) => {
+    const len = Object.keys(blogs).length
+    console.log("lenght", Object.keys(blogs).length)
+    if (len === 0) {
+        return 0
+    }
+    const likeArray = []
+    for (var i = 0; i < len; i++) {
+        likeArray.push(blogs[i].likes)
+    }
+    console.log("array to be reduced", likeArray)
+    const amount = likeArray.reduce((preVal, curVal) => preVal + curVal);
+    return amount
+}
+const favoriteblog = (blogs) => {
+    const len = Object.keys(blogs).length
+    console.log("lenght", Object.keys(blogs).length)
+    if (len === 0) {
+        return null
+    }
+    let mostLikes = 0
+    let favourite = null
+    for (var i = 0; i < len; i++) {
+        if (blogs[i].likes > mostLikes) {
+            favourite = i
+            mostLikes = blogs[i].likes
+        }
+    }
+    console.log(favourite)
+    if (favourite === null) {
+        return null
+    }
+    let _title = blogs[favourite].title
+    let _author = blogs[favourite].author
+    let _likes = blogs[favourite].likes
+    return {
+        title: _title,
+        author: _author,
+        likes: _likes
+    }
+}
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteblog
+}
