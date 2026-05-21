d9e59e46c6ca7a01fa474d0d268f2c100e6e5149
TuomasHirv <hirvonentume@gmail.com>
2026-05-21 17:56:06 +0300

    fix

---
+++ Osa 4/teht4.9-4.14/tests/blog_api.test.js
@@ -0,0 +1,117 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const blog = require('../models/blog')
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "Test title 1",
+    author: "Test user",
+    url: "https://fullstackopen.com",
+    likes: 0,
+  },
+  {
+    title: "Really useful blog",
+    author: "Second user",
+    url: "https://www.youtube.com/",
+    likes: 5,
+  },
+]
+
+
+beforeEach(async () => {
+    await blog.deleteMany({})
+    let blogObject = new blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
+
+test('blogs are returned correctly', async () => {
+    const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('blogs have a valid id', async () => {
+    const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    
+    response.body.forEach(res => {
+        assert.ok(res.id)
+        assert.ok(!('_id' in res))
+    })
+})
+
+test('blogs can be added', async () => {
+    const newBlog = {
+        title: "new blog",
+        author: "in test",
+        url: "http://localhost:3003/",
+        likes: 0,
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+    assert(titles.includes('new blog'))
+})
+
+test('likes defaults to 0', async () => {
+    const newBlog = {
+        title: "like test",
+        author: "in test",
+        url: "http://localhost:3003/",
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const testable = response.body.find(b => b.title === 'like test')
+
+    assert.strictEqual(testable.likes, 0)
+})
+
+test('missing title results in status 400', async () => {
+    const newBlog = {
+        author: "in test",
+        url: "http://localhost:3003/",
+        likes: 0,
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
